9e317a49b0cecb6f0d70d1103512036fd2bac891
gileduardo <gilgea@gmail.com>
2022-05-10 18:40:21 -0300

    spatie-permission installed

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // Gil Eduardo de Andrade
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
