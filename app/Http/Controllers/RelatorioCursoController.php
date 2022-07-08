bd1e1c26e748f59f7b890c86305389f573e1e8be
renanluizmacedo <renanluizmacedo@gmail.com>
2022-07-08 11:23:36 -0300

    verifica conceitos

--- app/Http/Controllers/RelatorioCursoController.php
+++ app/Http/Controllers/RelatorioCursoController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Curso;
 
 use App\Models\Conceito;
-use Illuminate\Support\Facades\DB;
 use Dompdf\Options;
 use Dompdf\Dompdf;
 use Illuminate\Support\Facades\App;
@@ -21,34 +20,42 @@ class RelatorioCursoController extends Controller
 
     public function gerarPDF($idCurso)
     {
+        $nomeCurso = Curso::find($idCurso);
+        $data = date('d-m-Y h', time());
+
         $infoCurso = $this->cursoSelecionado($idCurso);
-        //$HTML = $this->montarHtml($infoCurso);
+        $HTML = $this->montarHtml($infoCurso, $nomeCurso, $idCurso);
 
         $options = new Options();
         $options->set('isRemoteEnabled', true);
         $pdf = App::make('dompdf.wrapper');
-        //  $pdf->loadHTML($HTML);
-
-        //return $pdf->stream();
+        $pdf->loadHTML($HTML);
+        return $pdf->stream($nomeCurso->sigla . "-" . $data . '.pdf');
     }
-    /*
-    public function montarHtml($cursos)
+
+    public function montarHtml($infoCurso, $curso, $idCurso)
     {
-        $infoTd = $this->htlmTabelaInfoCurso($cursos);
+        $logo = './imgs/logoIF.png';
+        $grafico = $this->gerarGrafico($idCurso);
+
+        $infoTd = $this->htlmTabelaInfoCurso($infoCurso);
 
         $html =
             '
         <style>
         ' . file_get_contents("./css/pdf.css") . '
         </style>
-
+            <div class ="logo">
+            <img src="' . $logo . '">   
+        </div>
+        <h3>' . $curso->nome . '</h3>
+        <h3>Tempo de curso: ' . $curso->tempo . '  anos</h3>
         <table class="table ">
             <thead class = "thead">
                 <tr>
-                    <th>Conceito</td>
+                    <th>Aluno</td>
                     <th>Disciplina</td>
-                    <th>Professor</td>
-                    <th>Bimestre</td>
+                    <th>Conceito</td>
                     <th>Ano</td>
                 </tr>
             </thead>
@@ -56,7 +63,9 @@ class RelatorioCursoController extends Controller
             ' . $infoTd . '
             </tbody>
         </table>
-
+        <div class ="grafico">
+        ' . $grafico . '
+        </div>
     ';
 
         return $html;
@@ -64,49 +73,136 @@ class RelatorioCursoController extends Controller
     public function htlmTabelaInfoCurso($cursos)
     {
 
-        $tb =  "<tr>";
+
+        $tb =  "<tr class= 'tr_table'>";
 
         foreach ($cursos as $curso) {
-            $tb = "
-            <td>$curso->conceito</td>
-            <td>$curso->disciplina</td>
-            <td>$curso->professor</td>
-            <td>$curso->conceito</td>
-            ";
+            $tb .= "
+                <td class='td_table'>" . $curso->aluno->nome . "</td>
+                <td class='td_table'>" . $curso->disciplina->nome . "</td>
+                <td class='td_table'>" . $curso->conceito . "</td>
+                <td class='td_table'>" . $curso->ano . "</td>
+                </tr>";
         }
-
-        $tb .= "</tr>";
 
         return $tb;
     }
-    public function cursoSelecionado($idCurso)
-    {
-       $query = DB::table('conceitos')
-            ->join('alunos', 'conceitos.aluno_id', '=', 'id')
-            ->join('disciplinas', 'conceitos.disciplina_id', '=', 'disciplinas.id')
-            ->join('professors', 'conceitos.professor_id', '=', 'professors.id')
-            //->where('disciplinas.curso_id', '=', $idCurso)
-            //->where('alunos.curso_id', '=', $idCurso)
-            ->get();
 
-            dd($query);
-        //return $query;
-        
-    }*/
+
     public function cursoSelecionado($idCurso)
     {
-        
         $data = Conceito::with(
             [$disciplina = "disciplina" => function ($q) use ($idCurso) {
-                  return $q->where('disciplinas.curso_id', '=', $idCurso);
-
+                return $q->where('disciplinas.curso_id', '=', $idCurso);
             }],
         )->with(
             [$aluno = "aluno" => function ($q) use ($idCurso) {
-                 $q->where('alunos.curso_id', '=', $idCurso);
+                $q->where('alunos.curso_id', '=', $idCurso);
+                return $q->where('alunos.curso_id', '=', $idCurso);
             }],
-        )->get()->where($aluno ,'!=', null)->where($disciplina ,'!=', null);
+        )->get()->where($aluno, '!=', null)->where($disciplina, '!=', null);
 
         return $data;
+    }
+
+    public function gerarGrafico($idCurso)
+    {
+        $data = $this->cursoSelecionado($idCurso);
+
+
+        $contConceitos = ["A" => 0, "B" => 0, "C" => 0, "D" => 0];
+
+        function verificarConceitos($data, $contConceitos)
+        {
+            foreach ($data as $d) {
+                if ($d->conceito == 'A') {
+                    $contConceitos['A']++;
+                } elseif ($d->conceito == 'B') {
+                    $contConceitos['B']++;
+                } elseif ($d->conceito == 'C') {
+                    $contConceitos['C']++;
+                } elseif ($d->conceito == 'D') {
+                    $contConceitos['D']++;
+                }
+            };
+
+            $flagA = 0;
+            $flagB = 0;
+            $flagC = 0;
+            $flagD = 0;
+
+            for ($i = 0; $i < 4; $i++) {
+
+                if ($flagA == 0) {
+                    if ($contConceitos['A'] == 0) {
+                        unset($contConceitos['A']);
+                    }
+                    $flagA = 1;
+                } elseif ($flagB == 0) {
+                    if ($contConceitos['B'] == 0) {
+                        unset($contConceitos['B']);
+                    }
+                    $flagB = 1;
+                } elseif ($flagC == 0) {
+                    if ($contConceitos['C'] == 0) {
+                        unset($contConceitos['C']);
+                    }
+                    $flagC = 1;
+                } elseif ($flagD == 0) {
+                    if ($contConceitos['D'] == 0) {
+                        unset($contConceitos['D']);
+                    }
+                }
+            }
+            return $contConceitos;
+        }
+
+        $contConceitos = verificarConceitos($data, $contConceitos);
+
+        $client = new \GuzzleHttp\Client();
+
+        $chartData = [
+            "type" => 'doughnut',
+            "data" => [
+                "labels" => [],
+                "datasets" => [
+                    [
+                        "data" => [],
+                        "backgroundColor" => ['#4a1309', '#34094a', '#720039', '#2d5b00']
+                    ],
+                ],
+            ],
+        ];
+
+
+        function montarChartData($chartData, $contConceitos)
+        {
+
+
+            $keys = array_keys($contConceitos);
+
+            for ($i = 0; $i < count($keys); $i++) {
+                array_push($chartData['data']['labels'], $keys[$i]);
+            }
+
+            for ($i = 0; $i < count($contConceitos); $i++) {
+                $data[$i] = $contConceitos[$keys[$i]];
+            }
+            $chartData['data']['datasets'][0]['data'] = $data;
+
+            return $chartData;
+        }
+        $chartData = montarChartData($chartData, $contConceitos);
+
+        $chartData = json_encode($chartData);
+
+        $chartURL = "https://quickchart.io/chart?c=" . urlencode($chartData);
+
+        $res = $client->get($chartURL);
+        $content = (string) $res->getBody();
+
+        $chart = 'data:image/png;base64,' . base64_encode($content);
+
+        return '<img src="' . $chart . '" ';
     }
 }
